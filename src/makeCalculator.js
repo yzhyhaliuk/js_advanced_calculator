30c42485f428472b9b5b99b74b536edd4f5d027d
Yana <anazigaluk@gmail.com>
2024-11-13 10:15:01 +0200

    create calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,31 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {};
+
+  calculator.result = 0;
+
+  calculator.operate = (callback, number) => {
+    calculator.result = callback(calculator.result, number);
+
+    return calculator;
+  };
+
+  calculator.add = (a, b) => a + b;
+
+  calculator.subtract = (a, b) => a - b;
+
+  calculator.multiply = (a, b) => a * b;
+
+  calculator.divide = (a, b) => a / b;
+
+  calculator.reset = () => {
+    calculator.result = 0;
+
+    return calculator;
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
